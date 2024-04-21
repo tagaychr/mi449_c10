4b1826f58b828b92278179aa507d96c88bdcf8a2
tagaychr <94014924+tagaychr@users.noreply.github.com>
2024-04-21 06:12:16 +0000

    fix url

--- javascript/main.js
+++ javascript/main.js
@@ -36,7 +36,7 @@ function generateMap(city){
     console.log(city);
     //https://api.tomtom.com/map/1/tile/basic/main/10/130/450/.png?key=jVzPLfJYABiemFUjBRts5PqLY9VKWMr4
     //https://api.tomtom.com/map/1/tile/basic/main/10/512/512.png?key=jVzPLfJYABiemFUjBRts5PqLY9VKWMr4
-    const apiUrl = `https://api.tomtom.com/map/1/tile/basic/main/10/512/512.png?key=jVzPLfJYABiemFUjBRts5PqLY9VKWMr4`;
+    const apiUrl = `https://api.tomtom.com/map/1/tile/basic/night/0/0/0.png?key=jVzPLfJYABiemFUjBRts5PqLY9VKWMr4`;
 
     const img = document.createElement('img');
 
