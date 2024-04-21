d14af52eafd1aa5e7992521b64830eabe8252703
tagaychr <94014924+tagaychr@users.noreply.github.com>
2024-04-21 05:26:33 +0000

    ccc

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,9 @@
 
 document.getElementById("cityForm").addEventListener("submit", function(event) {
-    //event.preventDefault(); // Prevent the form from submitting traditionally
+    //event.preventDefault(); 
 
-    // Get the value of the input field
     var city = document.getElementById("cityInput").value;
 
-    // Call your JavaScript function here, passing the city value
     generateWeather(city);
     generateMap(city);
 });
@@ -13,6 +11,6 @@ document.getElementById("cityForm").addEventListener("submit", function(event) {
 function generateWeather(city) {
     alert("You entered: " + city);
 }
-function generateWeather(city){
+function generateMap(city){
     console.log(city);
 }
